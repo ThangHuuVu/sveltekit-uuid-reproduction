c82708cd341c6d3dbc843a90e8f4609891614de4
Thang Vu <thang.vu@binance.com>
2022-12-13 16:34:07 +0700

    Init

---
+++ src/routes/api/test/+server.js
@@ -0,0 +1,7 @@
+import * as uuid from 'uuid'
+import { v4 as uuidv4 } from "uuid";
+export const GET = async (event) => {
+  // console.log('uuid', uuid.v4())
+  console.log('uuidv4', uuidv4())
+
+}
